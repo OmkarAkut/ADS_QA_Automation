26bfe9979931e304f855740817125625d7c6b486
omkar <omkar@dev-tc1.dev.harrispaas.com>
2023-10-27 07:00:15 -0500

    adding costing, ucqc and all other remaining

--- src/test/java/webdriver/suites/TestRun_Suite.java
+++ src/test/java/webdriver/suites/TestRun_Suite.java
@@ -4,14 +4,24 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import webdriver.globalscripts.accessibilitytests.TestSuiteAXE;
+import webdriver.globalscripts.checktests.ChecktestsSuite;
 import webdriver.globalscripts.help.HelpSuite;
 import webdriver.globalscripts.pagetests.PagetestsSuite;
 import webdriver.globalscripts.securitytests.SecurityTestsSuite;
 import webdriver.scripts.contracting.ContractingSuite;
+import webdriver.scripts.costing.CostingTestSuite;
+import webdriver.scripts.costing.costingcalculations.CostingCalculationsTestSuite;
+import webdriver.scripts.costing.costingmodels.CostingModelsTestSuite;
+import webdriver.scripts.costing.unitcostquickcalculation.UnitCostQuickCalculationSuite;
+import webdriver.scripts.costing.unitcostquickcalculation.ucqccalculation.UcqcCalculationTestSuite;
+import webdriver.scripts.costing.unitcostquickcalculation.ucqcmainpage.UcqcMainpageTestSuite;
+import webdriver.scripts.costing.unitcostquickcalculation.ucqcrvus.UcqcRvusSuite;
 import webdriver.scripts.datamaintenance.maintaindata.MaintaindataTestSuite;
 import webdriver.scripts.datamaintenance.utilities.UtilitiesTestSuite;
 import webdriver.scripts.episodes.EpisodesTestSuite;
+import webdriver.scripts.regression.generalcalculations.GeneralCalculationsTestSuite;
 import webdriver.scripts.reporting.ReportingTestSuite;
+import webdriver.scripts.security.ucqcroles.UcqcRoleBasedTestSuite;
 import webdriver.scripts.systemmaintenance.SystemMaintenanceTestSuite;
 
 
@@ -30,6 +40,17 @@ import webdriver.scripts.systemmaintenance.SystemMaintenanceTestSuite;
 	PagetestsSuite.class,
 	HelpSuite.class,
 	SecurityTestsSuite.class,
+	ChecktestsSuite.class,
+	CostingTestSuite.class,
+	CostingCalculationsTestSuite.class,
+	CostingModelsTestSuite.class,
+	UnitCostQuickCalculationSuite.class,
+	UcqcCalculationTestSuite.class,
+	UcqcMainpageTestSuite.class,
+	UcqcRvusSuite.class,
+	UcqcRoleBasedTestSuite.class,
+	GeneralCalculationsTestSuite.class,
+	ReportingTestSuite.class,
 })
 
 public class TestRun_Suite {
