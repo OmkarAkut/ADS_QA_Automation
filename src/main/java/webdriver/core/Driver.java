8dc2e60caa4d000fd826586a75999f4454a2fb27
test_1234 <test_1234@dev-tc1.hadsscc-us.com>
2025-01-02 05:08:33 -0600

    DB url update

--- src/main/java/webdriver/core/Driver.java
+++ src/main/java/webdriver/core/Driver.java
@@ -366,6 +366,7 @@ public class Driver {
 //    	dbUrl = "jdbc:oracle:thin:@10.204.20.120:1522:qadb01"; //"jdbc:oracle:thin:@192.168.210.100:1540:qav8";
     //Shilpa update dburl for 11.2 on 30.4.2024
 //    	dbUrl = "jdbc:oracle:thin:@10.204.20.101:1529:qareborn";
+    	 //Shilpa update dburl for 11.2 on 2.1.2025
     	dbUrl = "jdbc:oracle:thin:@172.20.2.37:1750:qareborn";
     	System.out.println(dbUrl);
 
